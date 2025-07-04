a647ee6cbf233619385ce79f9f2604dccaaeda81
RoeiGlamDev <Roeiglam2@gmail.com>
2025-07-04 19:53:31 +0700

    Add pages/_app.js

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+// pages/_app.js
+import ErrorBoundary from '../components/ErrorBoundary';
+import * as Sentry from '@sentry/nextjs';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  );
+}
+
+export default MyApp;
